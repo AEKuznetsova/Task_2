a6e430cc5d02919d7604f01a0ab6406567a038e4
Anastasia <stasie89@mail.ru>
2026-03-14 22:53:38 +0300

    IngredientClient added

---
+++ src/test/java/client/IngredientClient.java
@@ -0,0 +1,12 @@
+package client;
+
+import io.restassured.response.Response;
+
+public class IngredientClient extends Client {
+    private static final String INGREDIENTS_ENDPOINT = "/ingredients";
+
+    public Response fetchAllIngredients() {
+        return specification()
+                .get(INGREDIENTS_ENDPOINT);
+    }
+}
