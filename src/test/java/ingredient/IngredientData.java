9f277b957672937bd617a4f19ecaeadf9a8c0806
Anastasia <stasie89@mail.ru>
2026-03-15 21:36:50 +0300

    Fixes according to comments made

--- src/test/java/ingredient/IngredientData.java
+++ src/test/java/ingredient/IngredientData.java
@@ -1,9 +1,12 @@
 package ingredient;
 
+import client.IngredientClient;
+import io.restassured.response.Response;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,23 +16,18 @@ import java.util.List;
 @EqualsAndHashCode
 public class IngredientData {
     private final List<Ingredient> ingredients = new ArrayList<>();
+    private final IngredientClient ingredientClient = new IngredientClient();
 
     public IngredientData() {
         putTestData();
     }
 
     private void putTestData() {
-        ingredients.add(new Ingredient(
-                "61c0c5a71d1f82001bdaaa6d",
-                "Флюоресцентная булка R2-D3",
-                "bun",
-                44, 26, 85, 643, 988,
-                "https://code.s3.yandex.net/react/code/bun-01.png",
-                "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-                "https://code.s3.yandex.net/react/code/bun-01-large.png",
-                0
-        ));
+        Response response = ingredientClient.fetchAllIngredients();
+        IngredientResponse ingredientResponse = response.as(IngredientResponse.class);
 
+        if (ingredientResponse != null && ingredientResponse.getData() != null) {
+            ingredients.addAll(ingredientResponse.getData());
+        }
     }
 }
-
